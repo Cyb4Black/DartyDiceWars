3cdfb5560c73cf05aae46990998ac167d0d13a93
Hex-3-En <pwillnow@gmail.com>
2015-07-04 23:01:36 +0200

    no message

--- bin/server.dart
+++ bin/server.dart
@@ -2,7 +2,7 @@ import 'package:start/start.dart';
 
 main(){
   start(port:8080).then((Server server){
-    server.static('../build/web', jail: false);
+    server.static('build/web', jail: false);
     
     
   });
